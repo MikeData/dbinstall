65c5b223094a3a8f74fea95b597609c79288c156
Michael Adams <mikelovesbooks@gmail.com>
2018-02-12 22:03:17 +0000

    Add files via upload

---
+++ install.cmd
@@ -0,0 +1,12 @@
+# Create environment
+conda create --name databaker python=3.5
+
+call activate databaker 
+
+pip install --upgrade pip
+
+pip install pandas
+pip install databaker
+pip install jupyter
+
+pause
